0949040ccc8767843528f6e8b4557cd87518dcc3
Larry Cousino <lawrence.cousino@gmail.com>
2020-04-12 17:35:48 -0400

    add STS4 package

---
+++ dealer_information/src/main/java/com/dealer_information/DealerInformationApplication.java
@@ -0,0 +1,13 @@
+package com.dealer_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerInformationApplication.class, args);
+	}
+
+}
---
+++ dealer_information/src/test/java/com/dealer_information/DealerInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealer_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
